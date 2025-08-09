d69b872db6b16c1b1ae099f327fcbd7cf073e430
youngers2006 <sy2224@ic.ac.uk>
2025-08-09 20:14:03 +0100

    added

--- SARSA_lambda.py
+++ SARSA_lambda.py
@@ -3,25 +3,27 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 import gymnasium as gym
 
-env = gym.make('FrozenLake-v1', is_slippery=True, render_mode='human')
-num_states = env.observation_space.shape[0]
-num_actions = env.action_space.shape[0]
+env = gym.make('FrozenLake-v1', is_slippery=True)
+num_states = env.observation_space.n
+num_actions = env.action_space.n
 
 seed = 42
-episodes = 100
-epsilon = 1
-ep_decay = 0.9
+episodes = 1000
+epsilon = 1.0
+ep_decay = 0.999
 gamma = 0.99
 lambda_ = 0.9
-Learn_Rate = 0.1
+Learn_Rate = 0.05
 
-rand_key = jax.random.PRNGKey(seed=seed)
+key = jax.random.PRNGKey(seed=seed)
 
-def policy(Q, state, epsilon):
-    rand = jax.random.uniform(key=rand_key, shape=(1,), minval=0.0, maxval=1.0)
+def policy(Q, state, epsilon, key):
+
+    key, subkey = jax.random.split(key)
+    rand = jax.random.uniform(key=subkey, shape=(1,), minval=0.0, maxval=1.0)
 
     if rand >= epsilon:
-        action = jnp.argmax(Q[state,:])
+        action = int(jnp.argmax(Q[state,:]))
     else:
         action = env.action_space.sample()
     
@@ -30,36 +32,78 @@ def policy(Q, state, epsilon):
 
 # Initialise Q(s,a) for all s,a pairs
 # Q is tabular and there are 16 states and 4 actions in each state
-Q = jax.random.uniform(key=rand_key,shape=(num_states,num_actions), minval=0.0, maxval=0.001)
+Q = jax.random.uniform(key=key,shape=(num_states,num_actions), minval=0.0, maxval=0.001)
 
 E = jnp.zeros(shape=(num_states,num_actions))
 
-for episode in episodes:
-    E = jnp.zeros_like(E)
-    initial_observation, info = env.reset(seed=42)
-    terminated = False
-    state = initial_observation
-    action = policy(Q, state, epsilon)
+reward_list = []
 
-    while not terminated:
+for episode in range(episodes):
+    E = jnp.zeros_like(E)
+    initial_observation, info = env.reset(seed=seed)
+    terminated = False
+    truncated = False
+    state = initial_observation
+    action = policy(Q, state, epsilon, key)
+    total_reward = 0
+
+    while not (terminated or truncated):
 
         observation, reward, terminated, truncated, info = env.step(action)
+        total_reward += reward
 
         state_prime = observation
-        action_prime = policy(Q, state_prime, epsilon)
+        action_prime = policy(Q, state_prime, epsilon, key)
 
         TD_error = reward + gamma * Q[state_prime,action_prime] - Q[state,action]
-        E[state,action] = E[state,action] + 1
+        Esa = E[state,action]
+        E = E.at[state,action].set(Esa + 1)
 
-        for s in num_states:
-            for a in num_actions:
-                Q[s,a] = Q[s,a] + Learn_Rate * TD_error * E[s,a]
-                E[s,a] = gamma * lambda_ * E[s,a]
+        for s in range(num_states):
+            for a in range(num_actions):
+                Qsa = Q[s,a]
+                Esa = E[s,a]
+                Q = Q.at[s,a].set(Qsa + Learn_Rate * TD_error * Esa)
+                E = E.at[s,a].set(gamma * lambda_ * Esa)
 
         state = state_prime
         action = action_prime
 
+    reward_list.append(total_reward)
     epsilon = epsilon * ep_decay
+    seed = seed + episode
+
+plt.plot(reward_list)
+
+
+reward_list_test = []
+
+env = gym.make('FrozenLake-v1', is_slippery=True, render_mode='human')
+
+rewinds = input('number of times to watch')
+
+for episode in range(rewinds):
+    initial_observation, info = env.reset(seed=seed)
+    terminated = False
+    truncated = False
+    state = initial_observation
+    action = policy(Q, state, epsilon, key)
+    total_reward = 0
+
+    while not (terminated or truncated):
+
+        observation, reward, terminated, truncated, info = env.step(action)
+        total_reward += reward
+
+        state_prime = observation
+        action_prime = policy(Q, state_prime, epsilon, key)
+
+        state = state_prime
+        action = action_prime
+
+    reward_list_test.append(total_reward)
+
+
 
 
         
