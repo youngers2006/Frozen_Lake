d62db4ad00929ee0bdffc9b8fd6e20ac20a38d81
youngers2006 <sy2224@ic.ac.uk>
2025-08-10 13:54:12 +0100

    added

--- SARSA_lambda.py
+++ SARSA_lambda.py
@@ -1,8 +1,42 @@
-import jax
-import jax.numpy as jnp
+import numpy as np
 import matplotlib.pyplot as plt
 import gymnasium as gym
 from tqdm import tqdm
+
+class Agent:
+    def __init__(self, epsilon, alpha, gamma, lambda_, action_space, num_a, num_s):
+        self.epsilon = epsilon
+        self.alpha = alpha
+        self.gamma = gamma
+        self.lambda_ = lambda_
+        self.action_space = action_space
+        self.num_actions = num_a
+        self.num_states = num_s
+        self.Q = np.random.uniform(
+            low=0.0,
+            high=0.01,
+            size=(num_s,num_a)
+        )
+        self.E_Trace = np.zeros(shape=(num_s,num_a))
+        self.trained = False
+
+    def select_action(self, state):
+        if np.random.uniform(low=0.0, high=1.0, size=(1,)) >= self.epsilon:
+            action = np.argmax(self.Q[state,:])
+        else:
+            action = self.action_space.sample()
+        return action
+
+    def update_Q_E(self, state, state_, action, action_, reward):
+        self.E_Trace[state, action] += 1
+        TD_error = reward + gamma * self.Q[state_, action_] - self.Q[state, action]
+        self.Q = self.Q + self.alpha * self.E * TD_error
+        self.E = self.gamma * self.lambda_ * self.E
+
+    def decay_epsilon(self, decay_rate):
+        self.epsilon = self.epsilon * decay_rate
+
+
 
 env = gym.make('FrozenLake-v1', is_slippery=True)
 num_states = env.observation_space.n
@@ -10,105 +44,50 @@ num_actions = env.action_space.n
 
 seed = 42
 episodes = 100
-epsilon = 1.0
-ep_decay = 0.9
+epsilon_I = 1.0
+epsilon_final = 0.01
+ep_decay = (epsilon_I - epsilon_final) / (episodes - 20)
 gamma = 0.99
 lambda_ = 0.9
 Learn_Rate = 0.05
 
-key = jax.random.PRNGKey(seed=seed)
-
-def policy(Q, state, epsilon, key):
-
-    key, subkey = jax.random.split(key)
-    rand = jax.random.uniform(key=subkey, shape=(1,), minval=0.0, maxval=1.0)
-
-    if rand >= epsilon:
-        action = int(jnp.argmax(Q[state,:]))
-    else:
-        action = env.action_space.sample()
-    
-    return action
-
-
-# Initialise Q(s,a) for all s,a pairs
-# Q is tabular and there are 16 states and 4 actions in each state
-Q = jax.random.uniform(key=key,shape=(num_states,num_actions), minval=0.0, maxval=0.001)
-
-E = jnp.zeros(shape=(num_states,num_actions))
-
 reward_list = []
+agent = Agent(
+    epsilon_I,
+    Learn_Rate,
+    gamma,
+    lambda_,
+    env.action_space,
+    num_actions,
+    num_states
+)
 
 for episode in tqdm(range(episodes), leave=False):
-    E = jnp.zeros_like(E)
     initial_observation, info = env.reset(seed=seed)
     terminated = False
     truncated = False
     state = initial_observation
-    action = policy(Q, state, epsilon, key)
+    action = agent.select_action(state)
     total_reward = 0
 
     while not (terminated or truncated):
-
-        observation, reward, terminated, truncated, info = env.step(action)
+        state_, reward, terminated, truncated, info = env.step(action)
         total_reward += reward
+        action_ = agent.select_action(state_)
+        agent.update_Q_E(
+            state,
+            state_,
+            action,
+            action_,
+            reward
+        )
 
-        state_prime = observation
-        action_prime = policy(Q, state_prime, epsilon, key)
-
-        TD_error = reward + gamma * Q[state_prime,action_prime] - Q[state,action]
-        Esa = E[state,action]
-        E = E.at[state,action].set(Esa + 1)
-
-        for s in range(num_states):
-            for a in range(num_actions):
-                Qsa = Q[s,a]
-                Esa = E[s,a]
-                Q = Q.at[s,a].set(Qsa + Learn_Rate * TD_error * Esa)
-                E = E.at[s,a].set(gamma * lambda_ * Esa)
-
-        state = state_prime
-        action = action_prime
+        state = state_
+        action = action_
 
     reward_list.append(total_reward)
-    epsilon = epsilon * ep_decay
+    agent.decay_epsilon(ep_decay)
     seed = seed + episode
 
 plt.plot(reward_list)
 plt.show()
-
-
-reward_list_test = []
-
-env = gym.make('FrozenLake-v1', is_slippery=True, render_mode='human')
-
-rewinds = int(input('number of times to watch'))
-
-for episode in range(rewinds):
-    initial_observation, info = env.reset(seed=seed)
-    terminated = False
-    truncated = False
-    state = initial_observation
-    action = policy(Q, state, epsilon, key)
-    total_reward = 0
-
-    while not (terminated or truncated):
-
-        observation, reward, terminated, truncated, info = env.step(action)
-        total_reward += reward
-
-        state_prime = observation
-        action_prime = policy(Q, state_prime, epsilon, key)
-
-        state = state_prime
-        action = action_prime
-
-    reward_list_test.append(total_reward)
-
-plt.plot(reward_list_test)
-plt.show()
-
-
-
-
-        
--- expectation_SARSA.py
+++ expectation_SARSA.py
@@ -30,15 +30,62 @@ class Agent:
         P_greedy = self.epsilon / self.num_actions
         P_non_greedy = 1 - self.epsilon + self.epsilon / self.num_actions
         policy = np.full(shape=(self.num_actions), fill_value=P_non_greedy)
+        policy[action] = P_greedy
         
         TD_error = reward + np.sum(policy * self.Q[state_,self.action_space]) - self.Q[state,action]
         self.Q[state,action] = self.Q[state,action] + self.alpha * TD_error
+    
+    def decay_ep(self, decay_rate):
+        self.epsilon = self.epsilon * decay_rate
+    
+# Hyper Params
+alpha = 0.05
+gamma = 0.99
+epsilon_I = 1.0
+epsilon_final = 0.01
+num_episodes = 1000
+ep_decay_rate = (epsilon_I - epsilon_final) / num_episodes
+seed = 42
 
-    def get_Q(self):
-        return self.Q
-
-        
+# Create environment
 env = gym.make('FrozenLake-v1', is_slippery=True)
 num_states = env.observation_space.n
 num_actions = env.action_space.n
+
+reward_list = []
+agent = Agent(
+    epsilon_I,
+    alpha,
+    gamma,
+    env.action_space,
+    num_actions,
+    num_states
+)
+
+for episode in range(num_episodes):
+    episode_reward = 0
+    state, info = env.reset(seed=seed)
+    terminated = False
+    truncated = False
+    action = agent.select_action(state)
+    while not (terminated or truncated):
+        state_, reward, terminated, truncated, info= env.step(action)
+        episode_reward += reward
+        action_ = agent.select_action(state_)
+        agent.update_Q(
+            state,
+            state_,
+            reward,
+            action,
+            action_
+        )
+        state = state_
+        action = action_
+
+    agent.decay_ep(ep_decay_rate)
+    reward_list.append(episode_reward)
+
+plt.plot(reward_list)
+plt.show()
+
 
