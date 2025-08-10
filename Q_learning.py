4ecf6700f2317a06f5cf21d0387b3759468afea1
youngers2006 <sy2224@ic.ac.uk>
2025-08-10 03:19:45 +0100

    added

--- Q_learning.py
+++
@@ -1,5 +0,0 @@
-import gymnasium as gym
-import jax
-import jax.numpy as jnp
-import tqdm
-import matplotlib.pyplot as plt
---
+++ expectation_SARSA.py
@@ -0,0 +1,42 @@
+import gymnasium as gym
+import numpy as np
+import tqdm
+import matplotlib.pyplot as plt
+
+class Agent:
+    def __init__(self, epsilon, alpha, gamma, action_space, num_a, num_s):
+        self.Q = np.zeros(shape=(num_s,num_a))
+        self.epsilon = epsilon
+        self.num_actions = num_a
+        self.num_states = num_s
+        self.alpha = alpha
+        self.gamma = gamma
+        self.action_space = action_space
+
+    def select_action(self, state):
+        P_greedy = self.epsilon / self.num_actions
+        P_non_greedy = 1 - self.epsilon + self.epsilon / self.num_actions
+
+        best_a = np.argmax(self.Q[state,:])
+
+        probabilities = np.full(P_non_greedy)
+        probabilities[best_a] += P_greedy
+
+        action = np.random.choice(a=self.action_space, p=probabilities)
+
+        return action
+
+    def update(self, state, state_, reward, action, action_):
+        P_greedy = self.epsilon / self.num_actions
+        P_non_greedy = 1 - self.epsilon + self.epsilon / self.num_actions
+        policy = np.full(shape=(self.num_actions), fill_value=P_non_greedy)
+        
+        TD_error = reward + np.sum(policy * self.Q[state_,self.action_space]) - self.Q[state,action]
+        self.Q[state,action] = self.Q[state,action] + self.alpha * TD_error
+
+        
+
+env = gym.make('FrozenLake-v1', is_slippery=True)
+num_states = env.observation_space.n
+num_actions = env.action_space.n
+
