3cf1b11387047231d03ab337b7c3409d97de95be
youngers2006 <sy2224@ic.ac.uk>
2025-08-14 00:05:22 +0100

    added

--- dyna_Q.py
+++ dyna_Q.py
@@ -2,6 +2,7 @@ import numpy as np
 import gymnasium as gym
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import random
 
 class Agent:
     def __init__(self, epsilon, alpha, gamma, num_s, num_a, action_space):
@@ -16,30 +17,18 @@ class Agent:
             high=0.01, 
             size=(num_s,num_a)
         )
-        self.model = np.zeros(
-            shape=(num_s,num_a,2)
-        )
-        self.visits = np.zeros(shape=(num_s, num_a))
+        self.model = {}
 
-    def update_model(self, state, action, reward, state_):
-        new_results = np.array([reward, state_])
-        if self.visits[state,action] == 0:
-            self.model[state,action,0] = new_results[0]
-            self.model[state,action,1] = new_results[1]
+    def update_model(self, state, action, reward, state_, terminated):
+        self.model[(state, action)] = (reward, state_, terminated)
+
+    def update_Q(self, state, state_, action, reward, terminated):
+        if terminated:
+            target = reward
         else:
-            self.model[state, action] = np.append(self.model[state, action], new_results, axis=1)
+            target = reward + self.gamma * np.max(self.Q[state_, :])
 
-    def update_visit_list(self, state, action):
-        if np.sum(self.visits) == 0:
-            self.visit_list = np.array([state, action])
-            self.visits[state,action] += 1
-        else:
-            self.visit_list = np.append(self.visit_list, np.array([state, action]), axis=1)
-            self.visits[state,action] += 1
-
-
-    def update_Q(self, state, state_, action, action_, reward):
-        TD_error = reward + self.gamma * np.max(self.Q[state_, :]) - self.Q[state, action]
+        TD_error = target - self.Q[state, action]
         self.Q[state,action] = self.Q[state,action] + self.alpha * TD_error
 
     def select_action(self, state):
@@ -53,26 +42,29 @@ class Agent:
         self.epsilon = max(epsilon_final, self.epsilon - decay_rate)
     
     def planning(self, num_iterations):
-        for n in range(num_iterations):
-            state = np.random.choice(self.visit_list[:,0]) # need to correct, get from all sampled model states
-            action = np.random.choice(self.visit_list[:,1])
-            reward, state_ = self.model[state,action]
-            TD_error = reward + self.gamma * np.max(self.Q[state_,:]) - self.Q[state,action]
-            self.Q[state,action] = self.Q[state,action] + self.alpha * TD_error
+        if not self.model:
+            return
+        
+        visited_sa_pairs = list(self.model.keys())
+
+        for _ in range(num_iterations):
+            state, action = random.choice(visited_sa_pairs) # need to correct, get from all sampled model states
+            reward, state_, terminated = self.model[(state,action)]
+            self.update_Q(state, state_, action, reward, terminated)
 
 env = gym.make('FrozenLake-v1', is_slippery=True)
 num_states = env.observation_space.n
 num_actions = env.action_space.n
 
 seed = 42
-episodes = 1000
+episodes = 20000
 epsilon_I = 1.0
-epsilon_final = 0.01
-ep_decay = (epsilon_I - epsilon_final) / (episodes - 20)
+epsilon_final = 0.001
+ep_decay = (epsilon_I - epsilon_final) / (episodes - 1000)
 gamma = 0.99
 Learn_Rate = 0.05
 action_space = env.action_space
-planning_steps = 50
+planning_steps = 10
 
 reward_list = []
 agent = Agent(
@@ -84,7 +76,7 @@ agent = Agent(
     action_space
 )
 
-for episode in range(episodes):
+for episode in tqdm(range(episodes), leave=False):
     state, info = env.reset(seed=seed)
     action = agent.select_action(state)
     episode_reward = 0
@@ -93,15 +85,29 @@ for episode in range(episodes):
     while not (terminated or truncated):
         state_, reward, terminated, truncated, info = env.step(action)
         action_ = agent.select_action(state_)
-        agent.update_Q(state,state_,action,action_,reward)
-        agent.update_model(state,action,reward, state_)
-        agent.update_visit_list(state,action)
+        agent.update_Q(state, state_, action, reward, terminated)
+        agent.update_model(state, action, reward, state_, terminated)
         agent.planning(planning_steps)
         episode_reward += reward
     reward_list.append(episode_reward)
     agent.decay_epsilon(ep_decay)
     state = state_
     action = action_
+    seed = seed + 1
+
+plt.plot(reward_list)
+plt.show()
+
+bin_size = 20
+bins = []
+for i in range(len(reward_list) // 20):
+    idx = i * 20
+    np_rew = np.array(reward_list[i:(i + bin_size)]) 
+    bin_i = np.sum(np_rew)
+    bins.append(bin_i)
+
+plt.plot(bins)
+plt.show()
 
 
 
